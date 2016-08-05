d812769ed411d4521c992ff9f994d716a811976e
angreebunny <angree.bunny@gmail.com>
2016-08-05 05:44:44 -0400

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -4,29 +4,26 @@ from data import m49_cn
 
 def lookup_countrycode(data):
 
-    if isinstance(data, int):
-        if data < 895:
-            county_name = m49_cn.get(data)
-            if not county_name:
-                # To improve data set log misses
-                pass
-            else:
-                return county_name
-
-    elif isinstance(data, basestring):
+    if isinstance(data, basestring):
         data = data.strip()
         if len(data) == 2:
             county_name = cc2_cn.get(data.upper())
             if not county_name:
-                # To improve data set log misses
-                pass
+                # To imporve data set log misses
             else:
                 return county_name
 
         elif len(data) == 3:
             county_name = cc3_cn.get(data.upper())
             if not county_name:
-                # To improve data set log misses
-                pass
+                # To imporve data set log misses
+            else:
+                return county_name
+
+    elif isinstance(data, int):
+        if data < 895:
+            county_name = m49_cn.get(data)
+            if not county_name:
+                # To imporve data set log misses
             else:
                 return county_name
