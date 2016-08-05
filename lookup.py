25b3a0bcd45886c9b3723e6cb699414e3e30374c
angreebunny <angree.bunny@gmail.com>
2016-08-05 04:24:16 -0400

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -1,1 +1,21 @@
+from data import cc2_cn
+from data import cc3_cn
 
+def lookup_countrycode(data):
+    if data:
+        data = data.strip()
+        if len(data) == 2:
+            county_name = cc2_cn.get(data.upper())
+            if not county_name:
+                # log misses to impove data set
+                pass
+            else:
+                return county_name
+
+        elif len(data) == 3:
+            county_name = cc3_cn.get(data.upper())
+            if not county_name:
+                # log misses to impove data set
+                pass
+            else:
+                return county_name
